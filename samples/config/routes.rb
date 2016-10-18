db17b0c6dc9dd8891caca02d06e6dec1868887f4
Chosko <ruben.caliandro@gmail.com>
2016-10-18 11:31:50 +0200

    updated routes to match rails5 conventions

--- samples/config/routes.rb
+++ samples/config/routes.rb
@@ -1,5 +1,5 @@
 MerchantSamples::Engine.routes.draw do
   post  "merchant/ipn_notify" => "merchant#ipn_notify", :as => :ipn_notify
-  match "merchant(/:action)",  :controller => "merchant", :as => :merchant
+  match "merchant(/:action)",  :controller => "merchant", :as => :merchant, via: [ :get, :post ]
   root :to => "merchant#index"
 end
